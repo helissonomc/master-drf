bd0dc0b5f69190421524102d41586b3d4e78e405
helissonmagalhaes <helisson.oliveira@e-comprei.com>
2022-06-17 16:42:07 -0300

    celery flower

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -48,6 +48,8 @@ THIRD_PARTY_APPS = [
     'phonenumber_field',
     'drf_yasg',
     'corsheaders',
+    'djcelery_email',
+
 ]
 
 LOCAL_APPS = ["core_apps.users", "core_apps.profiles", "core_apps.common"]
@@ -157,6 +159,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_URLS_REGEX = r'^/api/.*$'
 
 AUTH_USER_MODEL = 'users.User'
+
+CELERY_BROKER_URL = env('CELERY_BROKER')
+CELERY_RESULT_BACKEND = env('CELERY_BACKEND')
+CELERY_TIMEZONE = 'UTC'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 LOGGING = {
     'version': 1,
--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -7,3 +7,7 @@ DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-6be-aq^witr#6%3u6e%q-#&hy4rbbf=%dbzj!t%@b!evp-m403')
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
+EMAIL_HOST = env('EMAIL_HOST', default='mailhog')
+EMAIL_PORT = env('EMAIL_PORT')
